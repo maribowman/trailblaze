2b1d44a5f15e3b8f2f190708f5ff629e849e11fa
mari__ <bowman.marius@gmail.com>
2025-07-11 19:08:52 +0200

    add id as invisible to overview

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 COLUMNS = [
+    {"headerName": "ID", "field": "id", "hide": True},
     {"headerName": "Name", "field": "guy", "sortable": True, "checkboxSelection": True},
     {"headerName": "Artikel", "field": "item"},
     {"headerName": "Menge", "field": "quantity"},
